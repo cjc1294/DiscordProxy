52dfa5e5cf62eea4a1d3cefc03fe98dde0709c56
cjc1294 <cjc1294@rit.edu>
2020-10-16 22:06:16 -0400

    Interface function outline

---
+++ Common/DiscordInterface.py
@@ -0,0 +1,17 @@
+import discord
+
+class Interface:
+    def __init__(self, token):
+        pass
+
+
+    def run(self):
+        pass
+
+
+    def recv(self):
+        pass
+
+
+    def send(self, message):
+        pass
