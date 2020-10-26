5e81f7d8d60469f9a83a3c63dfd4a6e3b8a9918b
cjc1294 <cjc1294@rit.edu>
2020-10-25 22:16:05 -0400

    Interface: outline functionality

--- Common/DiscordInterface.py
+++ Common/DiscordInterface.py
@@ -12,8 +12,23 @@ class Interface:
 
 
     def recv(self):
+        """
+        Receieve a message.
+        Essentialy a helper function for on_message.
+        Acts like normal socket recieve, except 
+        byte length doesn't need to be specified.
+        """
+
+        ## Get message from queue. Messages are processed in on_message
         pass
 
 
     def send(self, message):
+        ## Format message(if message is string, convert to 
+        ## bytes, then add message start text and length)
+
+        ## Get communication channel
+
+        ## Send message
+        # Message sent in base64 encoded chunks
         pass
