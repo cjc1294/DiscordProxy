8826082b5882cb9606d6502995fd3a81d29e5bcd
cjc1294 <cjc1294@rit.edu>
2020-11-09 19:00:11 -0500

    Interface: implemented receiving

--- Common/DiscordInterface.py
+++ Common/DiscordInterface.py
@@ -11,18 +11,18 @@ class Interface(discord.Client):
         super().__init__()
         self.commChannelId = commChannelId
         self.recvQueue = asyncio.Queue()
+        self.incomingQueue = asyncio.Queue()
 
 
-    def recv(self):
+    async def recv(self):
         """
         Receieve a message.
         Essentialy a helper function for on_message.
-        Acts like normal socket recieve, except 
-        byte length doesn't need to be specified.
+        Acts like normal socket recieve, except byte length doesn't need to be specified.
         """
 
-        ## Get message from queue. Messages are processed in on_message
-        pass
+        ## Get message from queue. Messages are received in on_message, then processed in process_messages
+        return await self.recvQueue.get()
 
 
     async def send(self, message):
@@ -48,12 +48,46 @@ class Interface(discord.Client):
         # Message sent in base64 encoded chunks
         remaining = formattedMessage
         while len(remaining) > self.messageBlockSize:
-            await commChannel.send(formattedMessage[:self.messageBlockSize])
+            await commChannel.send(remaining[:self.messageBlockSize])
             remaining = remaining[self.messageBlockSize:]
 
         await commChannel.send(remaining)
-        print("[->] {}".format(message))
 
 
     async def on_ready(self):
         print(self.user.name + ": Logged in")
+        await self.process_messages()
+
+
+    async def on_message(self, message):
+        """
+        Bas64 decode incoming messages, and add them to the incoming queue
+        """
+        if message.author != self.user:
+            await self.incomingQueue.put(b64decode(message.content))
+
+
+    async def process_messages(self):
+        """
+        Piece messages back together from the incomingQueue, then add them to the recvQueue
+        """
+        while self.is_ready():
+            message = b""
+
+            block = await self.incomingQueue.get()
+            if not block.startswith(self.startHeader.encode()):
+                continue
+
+            lines = block.split(b"\n")
+
+            length = int(lines[1].split(b":")[1])
+            body = b"\n".join(lines[2:])
+
+            while len(body) < length:
+                length -= len(body)
+                message += body
+                body = await self.incomingQueue.get()
+
+            message += body
+
+            await self.recvQueue.put(message)
